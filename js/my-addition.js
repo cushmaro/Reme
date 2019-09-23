1b9f1734f2dbfdb163a3023fb96105f02fba1d90
eranhir <51129560+eranhir@users.noreply.github.com>
2019-09-23 15:43:20 +0300

    eran 23/9
    עצרתי בלסדר את דף יצירת הקבוצה, חלוקה לשני אגפים

---
+++ js/my-addition.js
@@ -0,0 +1,40 @@
+var modal = document.getElementById('id-modal');
+var addMember = document.getElementById('add-member');
+var modalCancelBtn =document.getElementById(  'cancel-member');
+
+
+
+function showModal() {
+  modal.style.display='block';
+}
+
+function hideModal(){
+  modal.style.display='none';
+}
+
+modalCancelBtn.addEventListener("click", hideModal);
+addMember.addEventListener("click", showModal);
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//display the uploaded image
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#my-img')
+        .attr('src', e.target.result)
+        .width(150)
+        .height(150);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
